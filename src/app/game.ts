dba8b75ada2bdd2d2c155cc551f3a8adfdd530dc
kaddykube <148755325+kaddykube@users.noreply.github.com>
2024-06-27 12:13:47 +0200

    add: game class

--- src/app/game.ts
+++ src/app/game.ts
@@ -1,4 +1,70 @@
-import { Direction, Snake } from "./snake";
+/* TYPES */
+
+import { Dir } from "fs";
+
+enum Direction {
+  LEFT = 1,
+  RIGHT,
+  UP,
+  DOWN,
+}
+
+type Anchor = { x: number; y: number; direction: Direction };
+
+/* CLASSES */
+const MAX_X = 20;
+const MAX_Y = 20;
+
+class Snake {
+  color: string;
+  position: { x: number; y: number };
+  direction: Direction;
+  score: number;
+  path: Anchor[];
+
+  constructor(color: string, x: number, y: number) {
+    this.color = color;
+    this.position = { x: x, y: y };
+    this.direction = Direction.RIGHT;
+    this.score = 10;
+    this.path = [];
+    this.path.push({ x: x, y: y, direction: this.direction });
+  }
+
+  calcPosition(direction: Direction): { x: number; y: number } {
+    let position = { x: this.position.x, y: this.position.y };
+
+    if (direction === Direction.LEFT) {
+      position.x = this.position.x - 1;
+    } else if (direction === Direction.RIGHT) {
+      position.x = this.position.x + 1;
+    } else if (direction === Direction.UP) {
+      position.y = this.position.y - 1;
+    } else if (direction === Direction.DOWN) {
+      position.y = this.position.y + 1;
+    }
+    return position;
+  }
+
+  set(direction: Direction) {
+    if (direction) {
+      // add current values to path array
+      let anchor: Anchor = {
+        x: this.position.x,
+        y: this.position.y,
+        direction: this.direction,
+      };
+      this.path.push(anchor);
+
+      if (this.path.length > this.score) {
+        this.path.shift();
+      }
+      // set new values
+      this.position = this.calcPosition(direction);
+      this.direction = direction;
+    }
+  }
+}
 
 class Bite {
   position: { x: number; y: number };
@@ -36,9 +102,13 @@ class Canvas {
     canvas.width = this.nrTilesX * this.tileSize;
     canvas.height = this.nrTilesY * this.tileSize;
     this.canvas = canvas;
-    this.context = context;
+    if (context) {
+      this.context = context;
+    }
     let game = document.getElementById("game");
-    game.appendChild(this.canvas);
+    if (game) {
+      game.appendChild(this.canvas);
+    }
   }
 
   drawLines() {
@@ -94,30 +164,76 @@ class Canvas {
   }
 }
 
-/* init game */
+class Game {
+  canvas: Canvas;
+  speed: number;
+  counter: number;
+  snake: Snake;
+  bite: Bite;
 
-const MAX_X = 20;
-const MAX_Y = 20;
-
-const run = () => {
-  const canvas = new Canvas(20, MAX_X + 1, MAX_Y + 1);
-  let bite = new Bite("#4C0062", 11, 10);
-  let snake = new Snake("#00ff00", 5, 5);
-
-  if (canvas) {
-    canvas.drawBite(bite);
-    canvas.drawSnake(snake);
+  constructor(canvas: Canvas, speed: number) {
+    this.canvas = canvas;
+    this.speed = 1500;
+    this.counter = 0;
+    this.snake = new Snake("#00ff00", 5, 5);
+    this.bite = new Bite("#4C0062", 11, 10);
   }
 
-  function update() {
-    canvas.clear();
+  setSnake(snake: Snake) {
+    this.snake = snake;
+  }
 
-    bite.set(
+  setBite(bite: Bite) {
+    this.bite = bite;
+  }
+
+  setDirection(direction: Direction) {
+    this.snake.set(direction);
+    this.update();
+  }
+
+  update() {
+    this.canvas.clear();
+
+    this.bite.set(
       Math.floor(Math.random() * MAX_X),
       Math.floor(Math.random() * MAX_Y)
     );
-    canvas.drawBite(bite);
-    canvas.drawSnake(snake);
+    this.canvas.drawBite(this.bite);
+    this.canvas.drawSnake(this.snake);
+  }
+}
+
+/* INIT */
+
+const run = () => {
+  // init canvas and game items
+
+  let activeGame = false;
+  const canvas = new Canvas(20, MAX_X + 1, MAX_Y + 1);
+
+  // scene
+
+  const game = new Game(canvas, 1000);
+
+  // control
+
+  const startButton: HTMLButtonElement = <HTMLButtonElement>(
+    document.getElementById("start")
+  );
+  const pauseButton: HTMLButtonElement = <HTMLButtonElement>(
+    document.getElementById("pause")
+  );
+
+  if (startButton && pauseButton) {
+    startButton.addEventListener("click", () => {
+      activeGame = !activeGame;
+      startButton.disabled = true;
+      game.update();
+    });
+    pauseButton.addEventListener("click", () => {
+      activeGame = !activeGame;
+    });
   }
 
   window.addEventListener("keydown", (event: KeyboardEvent) => {
@@ -127,31 +243,26 @@ const run = () => {
 
     switch (event.key) {
       case "ArrowDown":
-        if (snake) {
-          snake.set(Direction.DOWN);
-          update();
+        if (game) {
+          game.setDirection(Direction.DOWN);
         }
         break;
       case "ArrowUp":
-        if (snake) {
-          snake.set(Direction.UP);
-          update();
+        if (game) {
+          game.setDirection(Direction.UP);
         }
         break;
       case "ArrowLeft":
-        if (snake) {
-          snake.set(Direction.LEFT);
-          update();
+        if (game) {
+          game.setDirection(Direction.LEFT);
         }
         break;
       case "ArrowRight":
-        if (snake) {
-          snake.set(Direction.RIGHT);
-          update();
+        if (game) {
+          game.setDirection(Direction.RIGHT);
         }
         break;
       case " ":
-        update();
         break;
       default:
         return;
